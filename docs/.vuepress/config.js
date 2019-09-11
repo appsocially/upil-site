5678590f5759ac4cfbb1b22b2bf0fe1f9860f059
Jeremy <wallslide@users.noreply.github.com>
2019-09-11 12:37:47 +0900

    Add title/description

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   theme: './theme',
+  title: 'UPIL',
+  description: 'User Page Interaction Language',
   head: [
     ['link', { rel: 'icon', href: '/logo.png' }],
     ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css' }]
