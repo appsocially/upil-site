2852b207c0885c24aced99dc6da3f8a215fadcea
Jeremy <jeremy@wallslide.com>
2020-05-07 17:19:36 +0900

    Start ChatThemePlugin explanation

---
+++ docs/.vuepress/components/Figure.vue
@@ -0,0 +1,18 @@
+<template>
+  <figure align='center'>
+    <slot/>
+    <figcaption><i>{{ caption }}</i></figcaption>
+  </figure>
+</template>
+
+<script>
+export default {
+  props: ['caption'],
+}
+</script>
+
+<style scoped>
+    figure {
+        margin-bottom: 20px;
+    }
+</style>
