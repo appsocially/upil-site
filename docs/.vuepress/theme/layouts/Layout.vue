66b2094a2fe763ec4e65a21450db6637c4109867
Jeremy <jeremy@wallslide.com>
2020-07-23 16:23:47 +0900

    Fix auto-scroll on page load

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout />
+</template>
+
+<script>
+import Layout from "@vuepress/theme-default/layouts/Layout.vue";
+export default {
+  components: {
+    Layout,
+  },
+  mounted() {
+    // Fix scrolling to the referenced element upon initial page load
+    setTimeout(() => {
+      const hash = document.location.hash;
+      if (hash.length > 1) {
+        const id = hash.substring(1);
+        const element = document.getElementById(id);
+        if (element) element.scrollIntoView();
+      }
+    }, 1000);
+  },
+};
+</script>
